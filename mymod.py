d5960871fbe9d58e635c4b8b2b26e83a22a3dd7a
Reuven Lerner <reuven@lerner.co.il>
2024-09-25 20:53:26 +0300

    gitautosync update 64

--- mymod.py
+++ mymod.py
@@ -1,4 +1,4 @@
-print(f'About to start mymod')
+print(f'About to start {__name__}')
 
 x = 100
 
@@ -7,4 +7,4 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print(f'About to end mymod')
+print(f'About to end {__name__}')
