50f09c5d2734b9ce9bc7e97e93c7b4ee2f644686
Reuven Lerner <reuven@lerner.co.il>
2024-09-25 20:46:56 +0300

    gitautosync update 55

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
-
+def menu(options):
+    while True:
+        user_choice = input(f'Enter your choice {options}: ').strip()
+        if user_choice in options:
+            return user_choice
+        print(f'Invalid option {user_choice}; try again')
 
 def greet(name):
     return f'Hello, {name}!'
